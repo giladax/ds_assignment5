c19b62fd6b5220115b0e4020ca8db2b7626ef8f1
Gilad <giladkoch1@gmail.com>
2016-06-08 20:26:11 +0300

    Added pointIndex pair class

---
+++ assignment5/src/PointIndexPair.java
@@ -0,0 +1,23 @@
+
+public class PointIndexPair {
+	private Point p;
+	private int i;
+	public PointIndexPair(Point p, int i) {
+		this.p = p;
+		this.i = i;
+	}
+	public PointIndexPair(PointIndexPair other) {
+		this.p = new Point(other.p);
+		this.i = other.i;
+	}
+	public Point getpPoint(){
+		return this.p;
+	}
+	public int getIndex(){
+		return this.i;
+	}
+	
+	public void setIndex(int i) {
+		this.i = i;
+	}
+}
